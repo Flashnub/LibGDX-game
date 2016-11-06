a5ba3dc367b9d4fc2d7c2d4efd17771e37d6b6e0
Iyaz <Iyaz@DESKTOP-S025RL0>
2016-11-06 00:22:52 -0400

    World Object support

--- core/src/com/mygdx/game/model/worldObjects/LeverSettings.java
+++ core/src/com/mygdx/game/model/worldObjects/LeverSettings.java
@@ -1,5 +1,8 @@
 package com.mygdx.game.model.worldObjects;
 
-public class LeverSettings {
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 
+public class LeverSettings {
+	Array <Vector2> tileCoordinatesToModify;
 }
